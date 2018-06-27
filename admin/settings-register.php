3ac45da354a57189fdd8c39b48a6c8f3d3c74103
Jerry Chen <jerrychen017@gmail.com>
2018-06-27 16:28:27 -0400

    fix a bug caused by assigning a wrong field to swl_email id

--- admin/settings-register.php
+++ admin/settings-register.php
@@ -1,117 +1,119 @@
 <?php // Simple WPEngine Log - Register Settings
 
 // exit if file is called directly
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 
-	exit;
+    exit;
 
 }
 
 // register plugin settings
-function swl_register_settings() {
+function swl_register_settings()
+{
 
-	register_setting(
-		'swl_options',
-		'swl_options',
-		'swl_callback_validate_options'
-	);
+    register_setting(
+        'swl_options',
+        'swl_options',
+        'swl_callback_validate_options'
+    );
 
-	add_settings_section(
-		'swl_section_bucket',
-		'Set AWS Bucket',
-		'swl_callback_section_bucket',
-		'simple_wpengine_log'
-	);
+    add_settings_section(
+        'swl_section_bucket',
+        'Set AWS Bucket',
+        'swl_callback_section_bucket',
+        'simple_wpengine_log'
+    );
 
 // aws reigon
-add_settings_field(
-  'aws_reigon',
-  'Reigon',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'aws_reigon', 'label' => 'Your AWS Reigon' ]
-);
+    add_settings_field(
+        'aws_reigon',
+        'Reigon',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'aws_reigon', 'label' => 'Your AWS Reigon']
+    );
 
 // aws endpoint
-add_settings_field(
-  'aws_endpoint',
-  'AWS Endpoint',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'aws_endpoint', 'label' => 'Your AWS Endpoint' ]
-);
+    add_settings_field(
+        'aws_endpoint',
+        'AWS Endpoint',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'aws_endpoint', 'label' => 'Your AWS Endpoint']
+    );
 
 // aws access key id
-add_settings_field(
-  'aws_access_id',
-  'AWS Access ID',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'aws_access_id', 'label' => 'Your AWS Access ID' ]
-);
+    add_settings_field(
+        'aws_access_id',
+        'AWS Access ID',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'aws_access_id', 'label' => 'Your AWS Access ID']
+    );
 
 // aws secrete access key
-add_settings_field(
-  'aws_access_key',
-  'AWS Access Key',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'aws_access_key', 'label' => 'Your AWS Access Key' ]
-);
+    add_settings_field(
+        'aws_access_key',
+        'AWS Access Key',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'aws_access_key', 'label' => 'Your AWS Access Key']
+    );
 
 // desinated aws bucket name
-add_settings_field(
-  'aws_bucket_name',
-  'AWS Bucket Name',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'aws_bucket_name', 'label' => 'Your Desinated AWS Bucket Name' ]
-);
+    add_settings_field(
+        'aws_bucket_name',
+        'AWS Bucket Name',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'aws_bucket_name', 'label' => 'Your Desinated AWS Bucket Name']
+    );
 
 // WPE access log location
-add_settings_field(
-  'wpe_access_loc',
-  'WPEngine Current Access Log Location',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'wpe_access_loc', 'label' => 'Your WPEngine Current Access Log Location' ]
-);
+    add_settings_field(
+        'wpe_access_loc',
+        'WPEngine Current Access Log Location',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'wpe_access_loc', 'label' => 'Your WPEngine Current Access Log Location']
+    );
 
 // WPE error log location
-add_settings_field(
-  'wpe_error_loc',
-  'WPEngine Current Error Log Location',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'wpe_error_loc', 'label' => 'Your WPEngine Current Error Log Location' ]
-);
+    add_settings_field(
+        'wpe_error_loc',
+        'WPEngine Current Error Log Location',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'wpe_error_loc', 'label' => 'Your WPEngine Current Error Log Location']
+    );
 
 // email that will be sent notifications to
-add_settings_field(
-  'swl_email',
-  'Email Notifications',
-  'swl_callback_field_text',
-  'simple_wpengine_log',
-  'swl_section_bucket',
-  [ 'id' => 'wpe_error_loc', 'label' => 'Email Notifications After Each Successful Upload' ]
-);
+    add_settings_field(
+        'swl_email',
+        'Email Notifications',
+        'swl_callback_field_text',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'swl_email', 'label' => 'Email Notifications After Each Successful Upload']
+    );
 
 // frequency
-add_settings_field(
-		'swl_frequency',
-		esc_html__('Frequency', 'swl'),
-		'swl_callback_field_select',
-		'simple_wpengine_log',
-		'swl_section_bucket',
-		[ 'id' => 'swl_frequency', 'label' => esc_html__('Choose frequency for copying log files', 'simple_wpengine_log') ]
-	);
+    add_settings_field(
+        'swl_frequency',
+        esc_html__('Frequency', 'swl'),
+        'swl_callback_field_select',
+        'simple_wpengine_log',
+        'swl_section_bucket',
+        ['id' => 'swl_frequency', 'label' => esc_html__('Choose frequency for copying log files', 'simple_wpengine_log')]
+    );
 
 }
-add_action( 'admin_init', 'swl_register_settings' );
+
+add_action('admin_init', 'swl_register_settings');
