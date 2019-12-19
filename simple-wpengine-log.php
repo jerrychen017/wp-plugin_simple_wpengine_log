63e5a9235e67a8df0de259bcdd92b62d9984b17d
Guangrui Chen <gchen41@jhu.edu>
2019-12-19 18:04:17 -0500

    update license version

--- simple-wpengine-log.php
+++ simple-wpengine-log.php
@@ -6,13 +6,13 @@ Plugin URI:  https://plugin-planet.com/
 Author:      Guangrui Chen - Jerry
 Author URI:  http://guangruichen.com
 Version:     1.0
-License:     GPLv2 or later
-License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+License:     GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.txt
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version
-2 of the License, or (at your option) any later version.
+3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
